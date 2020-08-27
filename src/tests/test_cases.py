f0b9a9729f19fa5959e58bfd12221ee436c27195
burak <burak.dursunlar@hotmail.com>
2020-08-27 21:55:08 +0300

    add text fixture

--- src/tests/test_cases.py
+++ src/tests/test_cases.py
@@ -9,121 +9,126 @@ parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
 
+@pytest.fixture
 def filled_set():
     return OrderedSet(1, 2, 3, 4, 5, 6)
 
 
-class TestBasic:
-    def test_contains(self):
-        s = filled_set()
-        s.add(5)
-        assert 5 in s
+def test_contains(filled_set):
+    filled_set.add(5)
+    assert 5 in filled_set
 
-    def test_contains_any(self):
-        s = filled_set()
-        assert s.contains_any(7, 8, 4, 9, "str")
-        assert not s.contains_any(7, "str", 5.5)
 
-    def test_contains_all(self):
-        s = filled_set()
-        assert s.contains_all(1, 2, 3, 4, 5)
-        assert not s.contains_all(1, 2, 3, 4, 5, 5.5)
+def test_contains_any(filled_set):
+    assert filled_set.contains_any(7, 8, 4, 9, "str")
+    assert not filled_set.contains_any(7, "str", 5.5)
 
-    def test_remove(self):
-        s = OrderedSet()
-        s.update(1, 2, 3, 4, 5, "str")
-        s.remove(4)
-        print(s)
-        assert s == OrderedSet(1, 2, 3, 5, "str")
-        with pytest.raises(KeyError):
-            s.remove(-1)
 
-    def test_remove_first(self):
-        s = filled_set()
-        s.remove(1)
-        assert s[0] == 2
-        assert s == OrderedSet(2, 3, 4, 5, 6)
+def test_contains_all(filled_set):
+    assert filled_set.contains_all(1, 2, 3, 4, 5)
+    assert not filled_set.contains_all(1, 2, 3, 4, 5, 5.5)
 
-    def test_remove_last(self):
-        s = filled_set()
-        s.remove(6)
-        assert s[-1] == 5
 
-    def test_add_remove_mixed(self):
-        s = filled_set()
-        s.remove(1)
-        assert s[0] == 2
-        assert s == OrderedSet(2, 3, 4, 5, 6)
-        s.remove(6)
-        assert s == OrderedSet(2, 3, 4, 5)
-        s.add(7)
-        assert s == OrderedSet(2, 3, 4, 5, 7)
+def test_remove(filled_set):
+    filled_set.update(1, 2, 3, 4, 5, "str")
+    filled_set.remove_all(4, 6)
+    print(filled_set)
+    assert filled_set == OrderedSet(1, 2, 3, 5, "str")
+    with pytest.raises(KeyError):
+        filled_set.remove(-1)
 
-    def test_len(self):
-        s = OrderedSet()
-        s.add(1)
-        s.add(1)
-        s.add(1)
-        s.add(2)
-        s.add(3)
-        s.remove(1)
-        assert len(s) == 2
-        assert len(filled_set()) == 6
 
-    def test_len2(self):
-        s = OrderedSet()
-        s.update(1, 2, 1, 1, 1, 1, 3)
-        assert len(s) == 3
+def test_remove_first(filled_set):
+    filled_set.remove(1)
+    assert filled_set[0] == 2
+    assert filled_set == OrderedSet(2, 3, 4, 5, 6)
 
-    def test_index_access(self):
-        s = OrderedSet()
-        s.update(1, 2, 3, 4, 5, 6, 7)
-        assert s[4] == s[-3] == 5
 
-    def test_equality(self):
-        s, s2 = OrderedSet(), OrderedSet()
+def test_remove_last(filled_set):
+    filled_set.remove(6)
+    assert filled_set[-1] == 5
 
-        s.update(1, 2, 3, 4, 5, 6, 7)
-        s2.update(1, 2, 3, 4, 5, 6, 7)
 
-        assert s == s2
-        s2.remove(1)
-        s2.add(1)
-        assert not s == s2
+def test_add_remove_mixed(filled_set):
+    filled_set.remove(1)
+    assert filled_set[0] == 2
+    assert filled_set == OrderedSet(2, 3, 4, 5, 6)
+    filled_set.remove(6)
+    assert filled_set == OrderedSet(2, 3, 4, 5)
+    filled_set.add(7)
+    assert filled_set == OrderedSet(2, 3, 4, 5, 7)
 
-    def test_uniqueness(self):
-        s = filled_set()
-        s.update(1, 2, 3, 4, 5, 6, 1, 1, 1, 55)
-        s.remove(6)
-        assert s == OrderedSet(1, 2, 3, 4, 5, 55)
 
-    def test_add_unhashable(self):
-        with pytest.raises(TypeError):
-            OrderedSet().add([])
+def test_len(filled_set):
+    s = OrderedSet()
+    s.add(1)
+    s.add(1)
+    s.add(1)
+    s.add(2)
+    s.add(3)
+    s.remove(1)
+    assert len(s) == 2
+    assert len(filled_set) == 6
 
-    def test_drain(self):
-        s = filled_set()
-        total = 0
-        for num in s.drain():
-            total += num
-        assert total == 21
-        assert s.is_empty()
 
-    def test_drain2(self):
-        s = filled_set()
-        e = OrderedSet()
-        for item in s.drain():
-            e.add(item)
-        assert s.is_empty()
-        assert str(e) == "OrderedSet(1, 2, 3, 4, 5, 6)"
+def test_len2():
+    s = OrderedSet()
+    s.update(1, 2, 1, 1, 1, 1, 3)
+    assert len(s) == 3
 
-    def test_drain_reversed(self):
-        s = filled_set()
-        e = OrderedSet()
-        for item in s.drain(reverse=True):
-            e.add(item)
-        assert s.is_empty()
-        assert str(e) == "OrderedSet(6, 5, 4, 3, 2, 1)"
 
-    def test_formatting(self):
-        assert str(filled_set()) == "OrderedSet(1, 2, 3, 4, 5, 6)"
+def test_index_access():
+    s = OrderedSet()
+    s.update(1, 2, 3, 4, 5, 6, 7)
+    assert s[4] == s[-3] == 5
+
+
+def test_equality():
+    s, s2 = OrderedSet(), OrderedSet()
+
+    s.update(1, 2, 3, 4, 5, 6, 7)
+    s2.update(1, 2, 3, 4, 5, 6, 7)
+
+    assert s == s2
+    s2.remove(1)
+    s2.add(1)
+    assert not s == s2
+
+
+def test_uniqueness(filled_set):
+    filled_set.update(1, 2, 3, 4, 5, 6, 1, 1, 1, 55)
+    filled_set.remove(6)
+    assert filled_set == OrderedSet(1, 2, 3, 4, 5, 55)
+
+
+def test_add_unhashable():
+    with pytest.raises(TypeError):
+        OrderedSet().add([])
+
+
+def test_drain(filled_set):
+    total = 0
+    for num in filled_set.drain():
+        total += num
+    assert total == 21
+    assert filled_set.is_empty()
+
+
+def test_drain2(filled_set):
+    e = OrderedSet()
+    for item in filled_set.drain():
+        e.add(item)
+    assert filled_set.is_empty()
+    assert str(e) == "OrderedSet(1, 2, 3, 4, 5, 6)"
+
+
+def test_drain_reversed(filled_set):
+    e = OrderedSet()
+    for item in filled_set.drain(reverse=True):
+        e.add(item)
+    assert filled_set.is_empty()
+    assert str(e) == "OrderedSet(6, 5, 4, 3, 2, 1)"
+
+
+def test_formatting(filled_set):
+    assert str(filled_set) == "OrderedSet(1, 2, 3, 4, 5, 6)"
